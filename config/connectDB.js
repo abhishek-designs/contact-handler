6a10521881f518a9297984a4e36ba087bee845a7
abhishek-designs <akalyane980@gmail.com>
2023-05-18 12:20:20 +0530

    Latest commit

--- config/connectDB.js
+++ config/connectDB.js
@@ -5,7 +5,7 @@ const config = require("config");
 // Connect to mongoDB database
 module.exports = () => {
   mongoose.connect(
-    config.get("connectString"),
+    "mongodb+srv://abhishek:Abhi9584@todo.r8sg1ne.mongodb.net/?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
